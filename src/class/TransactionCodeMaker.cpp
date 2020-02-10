6541c021c3bc0d0b6c4929f8ceb3ee7ad7a6d9b1
SiyavashGh <siyavash.ghorbani@uoit.net>
2020-02-10 08:52:03 -0500

    transactions edits

--- src/class/TransactionCodeMaker.cpp
+++ src/class/TransactionCodeMaker.cpp
@@ -1,3 +1,5 @@
+#include <string>
+#include "TransactionEnums.h"
 #include "User.cpp"
 
 using namespace std;
@@ -6,9 +8,18 @@ class TransactionCodeMaker
 {
 private:
     /* data */
+    void sorter(User user, TransactionType tranType);
+
 public:
     TransactionCodeMaker(/* args */);
     ~TransactionCodeMaker();
+    void makeCreate(User createdUser);
+    void makeDelete(User deletedUser);
+    void makeLogout(User currentUser);
+    void makeAddCredit(User addedUser);
+    void makeRefund(User buyer, User seller);
+    void makeCreate(Item item, User seller);
+    void makeBid(Item item, User seller, User buyer);
 };
 
 TransactionCodeMaker::TransactionCodeMaker(/* args */)
@@ -17,4 +28,8 @@ TransactionCodeMaker::TransactionCodeMaker(/* args */)
 
 TransactionCodeMaker::~TransactionCodeMaker()
 {
+}
+
+makeCreate(User createdUser){
+/**/
 }
