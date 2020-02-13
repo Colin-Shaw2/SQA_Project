b451f61ec9c080458b6b8f4ee3daca69c5c2b3c2
Colin-Shaw2 <colin.shaw2@uoit.net>
2020-02-13 14:50:53 -0500

    return types to string and made methodsd static

--- src/class/TransactionCodeMaker.cpp
+++ src/class/TransactionCodeMaker.cpp
@@ -18,11 +18,11 @@ public:
     TransactionCodeMaker(/* args */);
     ~TransactionCodeMaker();
 
-    void makeCreate(string username, string userType, int availableCredit);
-    void makeDelete(string username, string userType, int availableCredit);
-    void makeLogout(string username, string userType, int availableCredit);
-    void makeAddCredit(string username, string userType, int availableCredit);
-    void makeRefund(string buyerUsername, string sellerUsername, string userType, int availableCredit);
-    void makeBid(string buyerUsername, string sellerUsername, string itemName, int bid);
-    void makeAdvertise(string buyerUsername, string sellerUsername, string itemName, int minimumBid);
+    static string makeCreate(string username, string userType, int availableCredit);
+    static string makeDelete(string username, string userType, int availableCredit);
+    static string makeLogout(string username, string userType, int availableCredit);
+    static string makeAddCredit(string username, string userType, int availableCredit);
+    static string makeRefund(string buyerUsername, string sellerUsername, string userType, int availableCredit);
+    static string makeBid(string buyerUsername, string sellerUsername, string itemName, int bid);
+    static string makeAdvertise(string buyerUsername, string sellerUsername, string itemName, int minimumBid);
 };
