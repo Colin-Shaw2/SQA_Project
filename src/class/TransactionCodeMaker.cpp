817ee119d7dab061dab9440e03f671cc9e20b697
Colin-Shaw2 <colin.shaw2@uoit.net>
2020-02-13 15:32:47 -0500

    added makeCreate function

--- src/class/TransactionCodeMaker.cpp
+++ src/class/TransactionCodeMaker.cpp
@@ -18,8 +18,27 @@ public:
     TransactionCodeMaker(/* args */);
     ~TransactionCodeMaker();
 
-    static string makeCreate(string username, string userType, int availableCredit);
-    static string makeDelete(string username, string userType, int availableCredit);
+    static string makeCreate(string username, string userType, int availableCredit){
+        string code = "";
+        code += "01_";
+        code += username;
+        //assuming username length of 15
+        for(int i = username.length; i<=15; i++){
+            code +="_";
+        }
+        code += "_";
+        code += userType;
+        code += "_";
+        for(int i = to_string(availableCredit).length; i<=9; i++){
+            code +="0";
+        }
+        code += to_string(availableCredit);
+        return code;
+    }
+
+    static string makeDelete(string username, string userType, int availableCredit){
+        
+    }
     static string makeLogout(string username, string userType, int availableCredit);
     static string makeAddCredit(string username, string userType, int availableCredit);
     static string makeRefund(string buyerUsername, string sellerUsername, string userType, int availableCredit);
