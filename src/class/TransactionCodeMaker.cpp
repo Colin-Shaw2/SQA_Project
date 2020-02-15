2a09fbb21cae7758da9ca2948f00677404aee8a6
Colin-Shaw2 <colin.shaw2@uoit.net>
2020-02-15 15:39:34 -0500

    refectored transaction code maker

--- src/class/TransactionCodeMaker.cpp
+++ src/class/TransactionCodeMaker.cpp
@@ -13,6 +13,52 @@ class TransactionCodeMaker
 private:
     /* data */
     //void sorter(User &user, transactionType tranType);
+    static const int userNameLength = 15;//TODO
+    static const int creditLength = 9;
+    static const int bidLength = 6;
+    static const int itemLength = 19;
+    static const int auctionLength = 3;
+
+    static string makeBufferedString(string input, int length){
+        string outStr = "";
+        outStr += input;
+        for (int i = input.length(); i <= length; i++)
+        {
+            outStr += "_";
+        }
+        return outStr;
+    } 
+    
+    static string makeBufferedInt(int input, int length){
+        string outStr = "";
+        for (int i = to_string(input).length(); i <= length; i++)
+        {
+            outStr += "0";
+        }
+        outStr += to_string(input);
+        return outStr;
+    } 
+
+    static string makeBufferedUserName(string input){
+        return makeBufferedString(input, userNameLength);
+    } 
+    
+
+    static string makeBufferedBid(int input){
+        return makeBufferedInt(input, bidLength);
+    } 
+
+    static string makeBufferedCredit(int input){
+        return makeBufferedInt(input, creditLength);
+    } 
+    
+    static string makeBufferedItem(string input){
+        return makeBufferedString(input, itemLength);
+    } 
+
+    static string makeBufferedAuction(int input){
+        return makeBufferedInt(input, auctionLength);
+    } 
 
 public:
     TransactionCodeMaker(/* args */);
@@ -22,20 +68,15 @@ public:
     {
         string code = "";
         code += "01_";
-        code += username;
-        //assuming username length of 15
-        for (int i = username.length(); i <= 15; i++)
-        {
-            code += "_";
-        }
+
+        code += makeBufferedUserName(username);
         code += "_";
+        
         code += userType;
         code += "_";
-        for (int i = to_string(availableCredit).length(); i <= 9; i++)
-        {
-            code += "0";
-        }
-        code += to_string(availableCredit);
+        
+        
+        code += makeBufferedCredit(availableCredit);
         return code;
     }
 
@@ -43,20 +84,15 @@ public:
     {
         string code = "";
         code += "02_";
-        code += username;
-        //assuming username length of 15
-        for (int i = username.length(); i <= 15; i++)
-        {
-            code += "_";
-        }
+
+        code += makeBufferedUserName(username);
         code += "_";
+
         code += userType;
         code += "_";
-        for (int i = to_string(availableCredit).length(); i <= 9; i++)
-        {
-            code += "0";
-        }
-        code += to_string(availableCredit);
+
+        
+        code += makeBufferedCredit(availableCredit);
         return code;
     }
 
@@ -64,20 +100,15 @@ public:
     {
         string code = "";
         code += "00_";
-        code += username;
-        //assuming username length of 15
-        for (int i = username.length(); i <= 15; i++)
-        {
-            code += "_";
-        }
+        
+        code += makeBufferedUserName(username);
         code += "_";
+
         code += userType;
         code += "_";
-        for (int i = to_string(availableCredit).length(); i <= 9; i++)
-        {
-            code += "0";
-        }
-        code += to_string(availableCredit);
+        
+        
+        code += makeBufferedCredit(availableCredit);
         return code;
     }
     
@@ -85,20 +116,15 @@ public:
     {
         string code = "";
         code += "06_";
-        code += username;
-        //assuming username length of 15
-        for (int i = username.length(); i <= 15; i++)
-        {
-            code += "_";
-        }
+        
+        code += makeBufferedUserName(username);
         code += "_";
+
         code += userType;
         code += "_";
-        for (int i = to_string(availableCredit).length(); i <= 9; i++)
-        {
-            code += "0";
-        }
-        code += to_string(availableCredit);
+        
+        
+        code += makeBufferedCredit(availableCredit);
         return code;
     }
     
@@ -107,27 +133,16 @@ public:
         string code = "";
         code += "05_";
 
-        code += buyerUsername;
-        //assuming username length of 15
-        for (int i = buyerUsername.length(); i <= 15; i++)
-        {
-            code += "_";
-        }
+        
+        code += makeBufferedUserName(buyerUsername);
         code += "_";
 
-        code += sellerUsername;
-        //assuming username length of 15
-        for (int i = sellerUsername.length(); i <= 15; i++)
-        {
-            code += "_";
-        }
+        
+        code += makeBufferedUserName(sellerUsername);
         code += "_";
 
-        for (int i = to_string(availableCredit).length(); i <= 9; i++)
-        {
-            code += "0";
-        }
-        code += to_string(availableCredit);
+        
+        code += makeBufferedCredit(availableCredit);
         return code;
     }
 
@@ -136,76 +151,42 @@ public:
         code += "04_";
 
         
-        code += itemName;
-        //assuming username length of 19
-        for (int i = itemName.length(); i <= 19; i++)
-        {
-            code += "_";
-        }
-        code += "_";
-
-        code += buyerUsername;
-        //assuming username length of 15
-        for (int i = buyerUsername.length(); i <= 15; i++)
-        {
-            code += "_";
-        }
-        code += "_";
-
-        code += sellerUsername;
-        //assuming username length of 15
-        for (int i = sellerUsername.length(); i <= 15; i++)
-        {
-            code += "_";
-        }
+        
+        code += makeBufferedItem(itemName);
         code += "_";
 
         
-        for (int i = to_string(bid).length(); i <= 6; i++)
-        {
-            code += "0";
-        }
-        code += to_string(bid);
+        code += makeBufferedUserName(buyerUsername);
+        code += "_";
+
+        
+        code += makeBufferedUserName(sellerUsername);
+        code += "_";
+        
+        code += makeBufferedCredit(bid);
 
         return code;
     }
+    
     static string makeAdvertise(string username, int daysToAuction, string itemName, int minimumBid){
         string code = "";
         code += "03_";
 
         
-        code += itemName;
-        //assuming username length of 19
-        for (int i = itemName.length(); i <= 19; i++)
-        {
-            code += "_";
-        }
-        code += "_";
+        code += makeBufferedItem(itemName);
 
-        code += username;
-        //assuming username length of 15
-        for (int i = username.length(); i <= 15; i++)
-        {
-            code += "_";
-        }
+        
+        code += makeBufferedUserName(username);
         code += "_";
 
 
-        //assuming length of 3
-        for (int i = to_string(daysToAuction).length(); i <= 3; i++)
-        {
-            code += "0";
-        }
+        
+        code += makeBufferedAuction(auctionLength);
         code += to_string(daysToAuction);
         code += "_";
 
-
-        //assuming length of 3
-        for (int i = to_string(minimumBid).length(); i <= 6; i++)
-        {
-            code += "0";
-        }
-        code += to_string(minimumBid);
+        
+        code += makeBufferedCredit(minimumBid);
 
         return code;
     }
