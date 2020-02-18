518164a5873eae08656b96c61539ba5b9c0f3343
Colin-Shaw2 <colin.shaw2@uoit.net>
2020-02-18 14:15:24 -0500

    added comments

--- src/class/TransactionEnums.h
+++ src/class/TransactionEnums.h
@@ -1,3 +1,6 @@
+/*  List of enums used to determine the type of transaction taking place
+ *
+*/
 #pragma once
 enum transactionType {
     CREATE, DELETE, LOGOUT, ADD_CREDIT, REFUND, ADVERTISE, BID
