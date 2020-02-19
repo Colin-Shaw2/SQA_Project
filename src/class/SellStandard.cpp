5da8527f4f79ea902c0187e0e40adabbb417af2f
Colin-Shaw2 <colin.shaw2@uoit.net>
2020-02-18 19:02:57 -0500

    changed comments in buy and sell users

--- src/class/SellStandard.cpp
+++ src/class/SellStandard.cpp
@@ -1,6 +1,6 @@
 /*********************************
-* fullstandard class is used to process *
-* actions done by an fullstandard user  *
+* sellstandard class is used to process *
+* actions done by an sellstandard user  *
 *********************************/
 
 #include "User.cpp"
@@ -10,7 +10,6 @@ using namespace std;
 class SellStandard : public User
 {
 private:
-    /* data */
 public:
     SellStandard(string name, int accountBalance):User(name, accountBalance, "SS"){
     }
@@ -18,22 +17,27 @@ public:
     ~SellStandard();
 
 
-   /*
-    * addCredit is used to raise User's accountBalance. The parameter for
-    * addCredit is int amount. Before raising User's balance, addCredit
-    * checks if the addition of amount to accountBalance is under 1,000,000.
-    * If it's over 1,000,000, the attempt is rejected and a message is printed
-    * If it's under 1,000,000, the amount is added to accountBalance
-    *
-    * @return void
-    */
+    /**
+     * addCredit is used to raise this user's accountBalance. Before raising this user's balance,
+     * addCredit checks if the addition of amount to accountBalance is under 1,000,000.
+     * If it's over 1,000,000, the attempt is rejected and a message is printed
+     * If it's under 1,000,000, the amount is added to accountBalance
+     * Writes to the users file
+     * 
+     * @param amount    the amount of credit to be added 
+     * @return void
+     */
     void addCredit(int amount){};
 
-    /*
-     * advertise is called to put up a new item. The parameters for advertise
-     * are string itemName, int minimumBid, and int daysToBid.
-     * advertise will create a new Item and and output a successful message
-     *
+    /**
+     * advertise is called to put up a new item.
+     * 
+     * advertise will create a new Item output a successful message
+     * Writes to the items file
+     * 
+     * @param itemName      name of item to be sold
+     * @param minimumBid    minimum bid on item
+     * @param daysToBid     days to bid
      * @return void
      */
     void advertise(string itemName, int minimumBid, int daysToBid){};
