c142a7ec9446c501e4dca09a4f95ef9d12dd729a
Colin-Shaw2 <colin.shaw2@uoit.net>
2020-02-21 19:06:34 -0500

    Added class to handle IO

--- src/class/main.cpp
+++ src/class/main.cpp
@@ -10,6 +10,7 @@
 #include "SellStandard.cpp"
 #include "Item.cpp"
 #include "TransactionCodeMaker.cpp"
+#include "FileIOHandler.cpp"
 #include <string>
 #include <iostream>
 #include <fstream>
@@ -195,62 +196,6 @@ void checkCreateNewUser(string username, string userType, int credit){};
  */
 void checkBid(string itemName, string username, int amount){};
 
-/**
- * getUser searches file for a user and returns a new user object
- *
- * @param username the user trying to be found
- * @param userFile the filename where the users are stored
- * @return a pointer to a user object if the user is found or NULL if no user is found
- */
-User *getUser(string username, string userFile)
-{
-  //buffer username
-  for (int i = username.length(); i < usernameLength; i++)
-  {
-    username += "_";
-  }
-
-  string inString;
-  ifstream inFile;
-
-  inFile.open(userFile);
-  if (!inFile)
-  {
-    cout << "Unable to open file";
-    exit(1); // terminate with error
-  }
-
-  // read file line by line
-  while (getline(inFile, inString))
-  {
-    if (inString.substr(0, usernameLength) == username)
-    {
-      cout << "Login successful!" << endl;
-      inFile.close();
-      string userType = inString.substr(16, 2);
-      if (userType == "AA")
-      {
-        return new Admin(username, stoi(inString.substr(19, 9)));
-      }
-      if (userType == "FS")
-      {
-        return new FullStandard(username, stoi(inString.substr(19, 9)));
-      }
-      if (userType == "BS")
-      {
-        return new BuyStandard(username, stoi(inString.substr(19, 9)));
-      }
-      if (userType == "SS")
-      {
-        return new SellStandard(username, stoi(inString.substr(19, 9)));
-      }
-    }
-  }
-
-  inFile.close();
-  cout << "We could not find that username" << endl;
-  return NULL;
-};
 
 /**
  * main uses a loop on cin to take in the inputs
@@ -266,9 +211,10 @@ int main(int argc, char const *argv[])
     exit(-1);
   }
 
-  string itemFile = argv[1];
-  string userFile = argv[2];
-  string transactionFile = argv[3];
+  FileIOHandler ioHandler = FileIOHandler(argv[1], argv[2], argv[3]);
+  // string itemFile = argv[1];
+  // string userFile = argv[2];
+  // string transactionFile = argv[3];
 
   inputState currentState = STATE_LOGGED_OUT;
   bool quit = false;
@@ -339,7 +285,7 @@ int main(int argc, char const *argv[])
     {
       if (isValidUsername(input))
       {
-        currentUser = getUser(input, userFile);
+        currentUser = ioHandler.getUser(input);
         if (currentUser != NULL)
         {
           currentState = STATE_WAITING;
