ebfcd244e4f9ff3e19d17ddc1add7691baf9d1dc
Colin-Shaw2 <colin.shaw2@uoit.net>
2020-02-28 18:41:36 -0500

    added script back

---
+++ specificTest.sh
@@ -0,0 +1,39 @@
+
+make compile
+
+defaultUserFile="TestDefaults/users.txt"
+echo $defaultUserFile
+defaultItemFile="TestDefaults/items.txt"
+echo $defaultItemFile
+testTable="TestResults/testTable.csv"
+echo "Running Tests..."
+
+testToRun="Tests/AddCredit/*/"
+
+for directory in $testToRun ; do
+    >"${directory}testTransactions.txt"
+    # runs main using input.txt and and outputs it to testOutputs.txt
+    # main.exe user item transaction
+    ./main.exe $defaultUserFile $defaultItemFile "${directory}testTransactions.txt" < "${directory}input.txt" > "${directory}testOutput.txt"
+    dos2unix "${directory}testOutput.txt"
+    dos2unix "${directory}output.txt"
+    dos2unix "${directory}testTransactions.txt"
+    dos2unix "${directory}transactionoutputs.txt"
+done
+echo "Checking..."
+echo "TEST NAME, SUCCESS?" >> $testTable
+
+for directory in $testToRun ; do
+    # compare to output files
+    if diff -q "${directory}testTransactions.txt" "${directory}transactionoutputs.txt"; then
+        #if true check other file
+        if diff -q "${directory}output.txt" "${directory}testOutput.txt"; then
+            echo "${directory}, true" >> $testTable
+        else
+        echo "${directory}, false" >> $testTable
+        fi
+    else
+    echo "${directory}, false" >> $testTable
+    fi
+
+done
